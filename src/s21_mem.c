9477ae96879d4a032693bb2d6bab9c41a040e897
archoldf <archoldf@student.21-school.ru>
2024-01-04 21:47:10 +0700

    add: clang-format

--- src/s21_mem.c
+++ src/s21_mem.c
@@ -1,12 +1,12 @@
 #include "s21_string.h"
 
-void *s21_memchr(const void *str, int c, s21_size_t n){
+void *s21_memchr(const void *str, int c, s21_size_t n) {
   const unsigned char *p = str;
   void *result = NULL;
   unsigned char value = (unsigned char)c;
-  
+
   for (s21_size_t i = 0; i < n; i++) {
-    if(p[i] == value){
+    if (p[i] == value) {
       result = (void *)(p + i);
       break;
     }
