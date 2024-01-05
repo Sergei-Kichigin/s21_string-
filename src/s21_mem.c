3851e65b399c41677d67b122fb448126d1e072f6
carlsonh <seroho@Seroho-PC.>
2024-01-05 15:01:31 +0700

    memcmp fix for structed rules

--- src/s21_mem.c
+++ src/s21_mem.c
@@ -28,12 +28,15 @@ void *s21_memset(void *str, int c, s21_size_t n) {
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
   const unsigned char *p1 = str1;
   const unsigned char *p2 = str2;
+  int result = 0;
 
   for (s21_size_t i = 0; i < n; i++) {
     if (p1[i] != p2[i]) {
-      return (int)(p1[i] - p2[i]);
+      result = (int)(p1[i] - p2[i]);
+      break;
     }
   }
 
-  return 0;
+  return result;
 }
+
